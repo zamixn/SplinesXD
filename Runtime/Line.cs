ad0c75883b232bc43034cb68eb20c20ff92d02b4
Žygimantas Kadiša <kadisa917@gmail.com>
2022-04-25 01:07:28 +0300

    Initial commit

---
+++ Runtime/Line.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrameworksXD.SplinesXD
+{
+    public class Line : MonoBehaviour
+    {
+        [SerializeField] public Vector3 p0;
+        [SerializeField] public Vector3 p1;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
